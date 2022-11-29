3b50ddf2b64b4ec7d59df70d72fa912218a21d4f
AliakbarNateghi <88666781+AliakbarNateghi@users.noreply.github.com>
2022-11-29 12:04:14 -0500

    Add files via upload

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,15 +1,7 @@
-
-# backend/urls.py
-
 from django.contrib import admin
-from django.urls import path, include                 # add this
-from rest_framework import routers                    # add this
-from backend.todo.views import TodoView                       # add this
-        
-router = routers.DefaultRouter()                      # add this
-router.register(r'todos', TodoView, 'todo')     # add this
-        
+from django.urls import path, include
+
 urlpatterns = [
-    path('admin/', admin.site.urls),           
-    path('api/', include(router.urls))                # add this
+    path('admin/', admin.site.urls),
+    path('api/', include('logic.urls')),
 ]
