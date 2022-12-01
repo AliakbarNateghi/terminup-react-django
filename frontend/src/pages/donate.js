10964ec8e430c69393ac268b4607df76741c748c
AliakbarNateghi <88666781+AliakbarNateghi@users.noreply.github.com>
2022-12-01 04:02:32 +0330

    Add files via upload

---
+++ frontend/src/pages/donate.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import '../App.css'
+import Sidebar from '../components/Sidebar'
+import { BiCoffeeTogo, BiDonateHeart, GrContact } from 'react-icons/all'
+
+const Donate = () => {
+    return (
+        <div>
+            <Sidebar />
+            <br></br>
+            <div style={{textAlign : 'center', marginTop : '10%'}}>
+                <h1 className='font-face-gm'>دوست عزیزم + </h1>
+                <h1 className='font-face-gm'>ممنون از دلگرمی بزرگ و انگیزه ای ک بهم میدی واسه ادامه + </h1>
+                <h1 className='font-face-gm'> از طریق لینک های زیر میتونی مستقیم حمایت مالی کنی یا منو یه کافه مهمون کنی + </h1>
+                <a href="https://coffeete.ir/lkbrntgh">
+                    <BiCoffeeTogo style={{fontSize : '70px', color : '#00704A'}} />
+                </a>
+                <a href='#'>
+                    <BiDonateHeart style={{fontSize : '70px', color : '##ff66ff '}} />
+                </a>
+                <br></br><br></br><br></br>
+                <h1 className='font-face-gm'>البته ک همه چی حمایت مالی نیست و میتونی با پیام قشنگت بهم انگیزه بده + </h1>
+                <Link to='/about'><GrContact style={{fontSize : '70px', color : '#0088'}}/></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Donate
---
+++ frontend/src/pages/register.js
@@ -0,0 +1,2 @@
+import React, { useState } from 'react';
+import Parse from 'parse/dist'
