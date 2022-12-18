46a1ebcf7e869e6c38e0787dd4c7453733b014dd
AliakbarNateghi <88666781+AliakbarNateghi@users.noreply.github.com>
2022-12-19 02:41:22 +0330

    Add files via upload

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+# from logic.views import register, login
+import sys
+
+sys.path.append('.')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('logic.urls')),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    # path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    # path('register/', register, name='register'),
+    # path('login/', login, name='login'),
 ]
