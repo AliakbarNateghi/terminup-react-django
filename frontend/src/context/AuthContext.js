6c2da485f519cf634aacf057176a7489d564c3cf
AliakbarNateghi <88666781+AliakbarNateghi@users.noreply.github.com>
2022-12-25 20:39:52 +0330

    Add files via upload

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -8,20 +8,25 @@ export default AuthContext;
 
 
 export const AuthProvider = ({children}) => {
-    let [authTokens, setAuthTokens] = useState(()=> localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null)
-    let [user, setUser] = useState(()=> localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null)
+    let [authTokens, setAuthTokens] = useState(() => localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null)
+    let [user, setUser] = useState(() => localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null)
     let [loading, setLoading] = useState(true)
 
     const navigate = useNavigate()
 
-    let loginUser = async (e )=> {
+    let loginUser = async (e)=> {
         e.preventDefault()
         let response = await fetch('http://127.0.0.1:8000/api/token/', {
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
             },
-            body:JSON.stringify({'username':e.target.username.value, 'password':e.target.password.value})
+            body:JSON.stringify({
+                                // username,
+                                // password
+                                'username': e.target.username.value,
+                                'password': e.target.password.value
+                                })
         })
         let data = await response.json()
 
@@ -30,10 +35,29 @@ export const AuthProvider = ({children}) => {
             setUser(jwt_decode(data.access))
             localStorage.setItem('authTokens', JSON.stringify(data))
             navigate('/')
-        }else{
+        } else {
             alert('Something went wrong!')
         }
     }
+
+    // const registerUser = async (username, password, password2) => {
+    //     const response = await fetch("http://127.0.0.1:8000/api/register/", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify({
+    //         username,
+    //         password,
+    //         password2
+    //     })
+    //     });
+    //     if (response.status === 201) {
+    //     history.push("/login");
+    //     } else {
+    //     alert("Something went wrong!");
+    //     }
+    // };
 
 
     let logoutUser = () => {
@@ -72,6 +96,9 @@ export const AuthProvider = ({children}) => {
 
     let contextData = {
         user:user,
+        // registerUser,
+        // setUser,
+        // setAuthTokens,
         authTokens:authTokens,
         loginUser:loginUser,
         logoutUser:logoutUser,
--- frontend/src/utils/PrivateRoute.js
+++ frontend/src/utils/PrivateRoute.js
@@ -1,14 +1,33 @@
-import { Route, Navigate, Routes } from 'react-router-dom'
-import { useContext } from 'react'
-import AuthContext from '../context/AuthContext'
+import { Route, Navigate, Routes, Outlet } from 'react-router-dom';
+import React, { useContext } from 'react';
+import AuthContext from '../context/AuthContext';
 
-const PrivateRoute = ({children, ...rest}) => {
-    let {user} = useContext(AuthContext)
-    return(
-        <Routes>
-            <Route {...rest}>{!user ? <Navigate to="/login" /> :   children}</Route>
-        </Routes>
-    )
-}
+export const PrivateRoute = ({ children }) => {
+        console.log('children :', children);
+        const { user } = useContext(AuthContext);
+        console.log('user :', user);
+        console.log('AuthContext :', AuthContext);
+
+        if (!user) {
+            return <Navigate to="/" />
+        }
+        return children;
+};
+
+// const PrivateRoute = ({children, ...rest}) => {
+//     let { user } = useContext(AuthContext)
+//     return(
+//         <Routes>
+//             <Route {...rest}>{!user ? <Navigate to="/login" /> : children}</Route>
+//         </Routes>
+//     )
+// }
+
+// const PrivateRoute = () => {
+//     let auth = {'token':false}
+//     return (
+//         auth.token ? <Outlet /> : <Navigate to='/login' />
+//     )
+// }
 
 export default PrivateRoute;
