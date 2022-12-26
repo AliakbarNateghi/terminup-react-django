19c6099b000279033d12665d239059a3ce24cb99
AliakbarNateghi <88666781+AliakbarNateghi@users.noreply.github.com>
2022-12-27 01:34:00 +0330

    Add files via upload

--- frontend/src/utils/PrivateRoute.js
+++ frontend/src/utils/PrivateRoute.js
@@ -1,33 +1,15 @@
-import { Route, Navigate, Routes, Outlet } from 'react-router-dom';
+import { Route, Link, Redirect } from 'react-router-dom';
 import React, { useContext } from 'react';
 import AuthContext from '../context/AuthContext';
 
-export const PrivateRoute = ({ children }) => {
-        console.log('children :', children);
-        const { user } = useContext(AuthContext);
-        console.log('user :', user);
-        console.log('AuthContext :', AuthContext);
 
-        if (!user) {
-            return <Navigate to="/" />
-        }
-        return children;
+const PrivateRoute = ({ children, ...rest }) => {
+    let { user } = useContext(AuthContext)
+    return(
+        <Route {...rest}>
+            {!user ? <Redirect to="/login" /> : children}
+        </Route>
+    )
 };
-
-// const PrivateRoute = ({children, ...rest}) => {
-//     let { user } = useContext(AuthContext)
-//     return(
-//         <Routes>
-//             <Route {...rest}>{!user ? <Navigate to="/login" /> : children}</Route>
-//         </Routes>
-//     )
-// }
-
-// const PrivateRoute = () => {
-//     let auth = {'token':false}
-//     return (
-//         auth.token ? <Outlet /> : <Navigate to='/login' />
-//     )
-// }
 
 export default PrivateRoute;
