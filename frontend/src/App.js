92f3ab63e1262238210df5b026aa6ae623e0e6c2
AliakbarNateghi <88666781+AliakbarNateghi@users.noreply.github.com>
2022-12-27 01:34:56 +0330

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,83 +1,29 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import PrivateRoute from './utils/PrivateRoute'
 import { AuthProvider } from './context/AuthContext'
 
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
-import { Fragment } from 'react';
-
-
+import Notfound from './pages/NotFound'
+import Register from './pages/RegisterPage';
 
 function App() {
 
   return (
-    <div className='App'>
-        <Routes>
-          <Route path='/login' element={ <LoginPage /> } />
-          <Route path='/' element={
-                                    <PrivateRoute>
-                                      <HomePage />
-                                    </PrivateRoute>
-                                  }
-          />
-        </Routes>
+    <div className="App">
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <PrivateRoute component={HomePage} path="/" exact />
+            <Route component={Register} path="/register" />
+            <Route component={LoginPage} path="/login" />
+            <Route component={Notfound} path="*" />
+          </Switch>
+        </AuthProvider>
+      </Router>
     </div>
-  )
-
-  // return (
-  //   <div className='App'>
-  //     <Router>
-  //       <Routes>
-  //         <Route element={ <PrivateRoute /> } >
-  //           <Route element={ <HomePage /> } path='/' exact />
-  //         </Route>
-  //         <Route element={ <LoginPage /> } path='/login' />
-  //       </Routes>
-  //     </Router>
-  //   </div>
-  // )
-  
-  // return (
-  //   <div className="App">
-  //     <BrowserRouter>
-  //       <Fragment>
-  //         <AuthProvider>
-  //           <Routes>
-  //             <Route exact path='/' element={<PrivateRoute />} >
-  //               <Route exact path='/' element={<HomePage />} />
-  //             </Route>
-  //             <Route element={<LoginPage />} path="/login" />
-  //           </Routes>
-  //         </AuthProvider>
-  //       </Fragment>
-  //     </BrowserRouter>
-  //   </div>
-  // );
-
-  // return (
-  //   <div className='App'>
-  //     <BrowserRouter>
-  //       <AuthProvider>
-  //         <PrivateRoute component={HomePage} path="/" exact />
-  //         <Routes>
-  //           <Route component={LoginPage} path="/login" />
-  //         </Routes>
-  //       </AuthProvider>
-  //     </BrowserRouter>
-  //   </div>
-  // )
-
-  // return (
-  //   <div className="App">
-  //     <BrowserRouter>
-  //       <AuthProvider>
-  //         <PrivateRoute component={HomePage} path="/" exact/>
-  //         <Route component={LoginPage} path="/login"/>
-  //       </AuthProvider>
-  //     </BrowserRouter>
-  //   </div>
-  // );
+  );
 
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,52 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Route, Routes, Router, BrowserRouter } from 'react-router-dom';
-import About from './pages/about';
-import Donate from './pages/donate';
 import './pages/soltan-adan/soltanAdan.ttf';
-import Register from './components/register';
-import Login from './components/login';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>     
+    <App />
   </React.StrictMode>,
 );
-
-// function GeneralApp () {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path='/' element={<App />} />
-//         <Route path='/about' element={<About />} />
-//         <Route path='/donate' element={<Donate />} />
-//         <Route path='/register' element={<Register />} />
-//         <Route path='/login' element={<Login />} />
-//         {/* <Route exact path={"/home"} element={<Home />} />
-//         <Route exact path="/login" element={<Login />} />
-//         <Route exact path="/register" element={<Register />} />
-//         <Route exact path="/profile" element={<Profile />} />
-//         <Route path="/user" element={<BoardUser />} />
-//         <Route path="/mod" element={<BoardModerator />} /> */}
-//         {/* <Route path="/admin" element={<BoardAdmin />} /> */}
-//         {/* <Route path='/register' element={<UserRegistration />} />
-//         <Route path='/login' element={<UserLogin />} /> */}
-//       </Routes>
-//     </BrowserRouter>
-//   )
-// }
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     {/* <BrowserRouter> */}
-//       <GeneralApp />
-//     {/* </BrowserRouter> */}
-//   </React.StrictMode>
-// );
 
 reportWebVitals();
