45c9a00c7552e21aae45dcf8926a3b444235fa2b
AliakbarNateghi <88666781+AliakbarNateghi@users.noreply.github.com>
2022-12-27 01:33:28 +0330

    Add files via upload

---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { elastic as Menu } from 'react-burger-menu';
+import '../Sidebar.css';
+import About from '../pages/about';
+import { Link, Route, Routes, Router, BrowserRouter } from "react-router-dom";
+
+export default props => {
+  return (
+    <Menu>
+      <Link to='/'>
+        صفحه اصلی
+      </Link>
+      <Link to='/save'>
+        ذخیره برنامه
+      </Link>
+      <Link to='/donate'>
+        ساپورت مالی
+      </Link>
+      <Link to="/about">
+        درباره من
+      </Link>
+      {/* <a className="menu-item" href="/logout">
+        خروج
+      </a> */}
+
+      {/* <Outlet /> */}
+    </Menu>
+  );
+};
