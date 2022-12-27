dacfe6868e25ff0b32c62a3bdaaa840aeac8629a
AliakbarNateghi <88666781+AliakbarNateghi@users.noreply.github.com>
2022-12-28 01:56:32 +0330

    Add files via upload

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -2,28 +2,41 @@ import React from 'react';
 import { elastic as Menu } from 'react-burger-menu';
 import '../Sidebar.css';
 import About from '../pages/about';
-import { Link, Route, Routes, Router, BrowserRouter } from "react-router-dom";
+import { Link, Route, BrowserRouter, useHistory, Redirect } from "react-router-dom";
+import { AuthProvider } from '../context/AuthContext';
+
+// let Logout = AuthProvider.
+
+const HomeRedirect = () => {
+  let navigate = useHistory()
+  navigate.push('/donate');
+  window.location.reload();
+  // return (<Redirect to='/donate' />)
+  // window.location.reload()
+}
 
 export default props => {
   return (
+    
     <Menu>
+      {/* <div>
+        <p onClick={HomeRedirect} >Home</p>
+      </div> */}
       <Link to='/'>
         صفحه اصلی
       </Link>
       <Link to='/save'>
         ذخیره برنامه
       </Link>
-      <Link to='/donate'>
+      <p onClick={HomeRedirect}>
         ساپورت مالی
-      </Link>
+      </p>
       <Link to="/about">
         درباره من
       </Link>
-      {/* <a className="menu-item" href="/logout">
+      <p>
         خروج
-      </a> */}
-
-      {/* <Outlet /> */}
+      </p>
     </Menu>
   );
 };
